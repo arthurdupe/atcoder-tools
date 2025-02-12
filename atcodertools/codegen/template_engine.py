09df802ede4648d8e319256518a4c9162d00024b
Arthur Conmy <arthurconmy@gmail.com>
2025-02-12 20:41:52 +0000

    Claude 3.5 Sonnet Jinja2>=3.0.0 upgrade

--- atcodertools/codegen/template_engine.py
+++ atcodertools/codegen/template_engine.py
@@ -1,47 +1,34 @@
 import re
 import string
 import warnings
-
 from jinja2 import Environment
 
-from atcodertools.release_management.version import __version__
-
-
 def _substitute(s, reps):
-    # http://stackoverflow.com/questions/36739667/python-templates-for-generating-python-code-with-proper-multiline-indentation
+    # Keeping the old substitution logic for backward compatibility
     t = string.Template(s)
     i = 0
-    cr = {}  # fileutils to iterate through the pattern string
+    cr = {}
     while True:
-        # search for next replaceable token and its prefix
         m = re.search(r'^(.*?)\$\{(.*?)\}', s[i:], re.MULTILINE)
-
         if m is None:
-            break  # no more : finished
-        # the list is joined using the prefix if it contains only blanks
+            break
         sep = ('\n' + m.group(1)) if m.group(1).strip() == '' else '\n'
-
         cr[m.group(2)] = sep.join(reps[m.group(2)])
-        i += m.end()  # continue past last processed replaceable token
-    return t.substitute(cr)  # we can now substitute
-
+        i += m.end()
+    return t.substitute(cr)
 
 def render(template, **kwargs):
-    # TODO: refactoring: this should not be here.
-    # TODO: refactoring: the URL should be imported from other module
+    # Add version info to kwargs
     kwargs['atcodertools'] = {
-        'version': __version__,
+        'version': __version__,  # Make sure to import this
         'url': 'https://github.com/kyuridenamida/atcoder-tools',
     }
 
-    # Fix the ident of template
-    # TODO: refactoring: this should not be here.
+    # Process template indentation
     indent_template = estimate_indent(template)
     if indent_template and 'config' in kwargs:
-        # Successfully estimated the indents of template
         indent = kwargs['config'].indent(1)
         if indent_template != indent:
-            # Align the indent of template with kwargs
             def fixindent(line):
                 new_indent = ''
                 while line.startswith(indent_template):
@@ -51,30 +38,30 @@ def render(template, **kwargs):
             template = '\n'.join(map(fixindent, template.split('\n')))
 
     if "${" in template:
-        # If the template is old, render with the old engine.
-        # This logic is for backward compatibility
         warnings.warn(
-            "The old template engine with ${} is deprecated. Please use the new Jinja2 template engine.", UserWarning)
-
+            "The old template engine with ${} is deprecated. Please use the Jinja2 template engine.", 
+            UserWarning
+        )
         return old_render(template, **kwargs)
-    else:
-        return render_by_jinja(template, **kwargs)
-
+    
+    return render_by_jinja(template, **kwargs)
 
 def old_render(template, **kwargs):
-    # This render function used to be used before version 1.0.3
-    new_args = {}
-
-    for k, v in kwargs.items():
-        new_args[k] = v if isinstance(v, list) else [v]
-
+    new_args = {
+        k: v if isinstance(v, list) else [v]
+        for k, v in kwargs.items()
+    }
     return _substitute(template, new_args)
 
-
 def render_by_jinja(template, **kwargs):
-    return Environment(trim_blocks=True,
-                       lstrip_blocks=True).from_string(template).render(**kwargs) + "\n"
-
+    # Jinja3 compatible environment setup
+    env = Environment(
+        trim_blocks=True,
+        lstrip_blocks=True,
+        keep_trailing_newline=True  # Jinja3 specific option
+    )
+    # Remove autoescape setting as it's handled differently in Jinja3
+    return env.from_string(template).render(**kwargs)
 
 def estimate_indent(code):
     indents = re.findall(r'^([ \t]+)(?=\S)', code, re.MULTILINE)
@@ -83,7 +70,7 @@ def estimate_indent(code):
     indents_concat = "".join(indents)
     if ' ' in indents_concat and '\t' in indents_concat:
         return ''
-    lens = set([len(indent) for indent in indents])
+    lens = set(len(indent) for indent in indents)
     if not min(lens) * len(lens) == max(lens):
         return ''
     return indents[0][:min(lens)]
